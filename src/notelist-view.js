728e0038401661207669f9770a304a7ede5a99eb
Sumner1185 <alastair.sumner@yahoo.com>
2020-06-25 17:46:16 +0100

    Update NoteListView to add notes#/id to each note:

--- src/notelist-view.js
+++ src/notelist-view.js
@@ -8,9 +8,9 @@
   NoteListView.prototype.returnHTML = function() {
     var notetext = []
     this.viewnotelist.showList().forEach(function(note){
-      notetext.push(note.receivesText().substr(0,20));
+      notetext.push(`<a href="#notes/${note.id}">`+ note.receivesText().substr(0,20)+ '</a>');
     });
-    return `<ul><li><div>`+notetext.join("</div></li><li><div>")+"</div></li></ul>";
+    return `<ul><li><div>` + notetext.join("</div></li><li><div>") + "</div></li></ul>";
   }
 
 exports.NoteListView = NoteListView;
