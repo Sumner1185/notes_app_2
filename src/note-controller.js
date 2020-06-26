c2e7e80a76235326250f6e91418d58b0cc2e7be7
Sumner1185 <alastair.sumner@yahoo.com>
2020-06-26 15:43:33 +0100

    Single not HTML now rendering on hash change

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,6 +1,9 @@
 (function (exports) {
   function NoteController(noteList) {
-    this.noteList = noteList;
+    this.noteList = new NoteList();
+    this.noteList.addNote("Test0owuiehowheoiwheoiuwefwerfeifjho23", 0);
+    this.noteList.addNote("Test1powiehjfowuhefouwhefouhwef", 1);
+    this.noteList.addNote("Testpiwjefoiwoeih2oiefho2iehf2", 2);
     this.noteListView = new NoteListView(this.noteList);
   }
 
@@ -9,21 +12,27 @@
   };
 
   NoteController.prototype.listener = function () {
-    window.addEventListener("hashchange", displayNote);
+    window.location.addEventListener("hashchange", this.returnIdFromURL());
   };
 
   NoteController.prototype.returnIdFromURL = function () {
-    return parseInt(window.location.hash.split("/")[1]);
+    var id = parseInt(window.location.hash.split("/")[1]);
+    this.returnSingleNote(id);
   };
 
   NoteController.prototype.returnSingleNote = function (id) {
     let note = this.noteList.returnNoteById(id);
-    return note;
+    this.returnSingleNoteHtml(note);
   };
 
   NoteController.prototype.returnSingleNoteHtml = function (note) {
     let singleNote = new SingleNoteView(note);
-    return singleNote.returnHtmlString();
+    var html = singleNote.returnHtmlString();
+    this.renderSingleNoteHtml(html);
+  };
+
+  NoteController.prototype.renderSingleNoteHtml = function (html) {
+    document.getElementById("app").innerHTML = html
   };
 
   exports.NoteController = NoteController;
